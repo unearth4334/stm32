c057ccd366531b3c4e39057ced3d8dd5475a8949
Stefan Damkjar <stefan.damkjar@redlen.com>
2026-03-24 15:07:30 -0700

    fix(rtos): add SVC and PendSV handlers

--- projects/blackpill_f411ce_blinky/stm32f4xx_it.c
+++ projects/blackpill_f411ce_blinky/stm32f4xx_it.c
@@ -3,6 +3,21 @@
 #if defined(USE_FREERTOS)
 #include "FreeRTOS.h"
 #include "task.h"
+
+extern void vPortSVCHandler(void);
+extern void xPortPendSVHandler(void);
+#endif
+
+#if defined(USE_FREERTOS)
+void SVC_Handler(void)
+{
+    vPortSVCHandler();
+}
+
+void PendSV_Handler(void)
+{
+    xPortPendSVHandler();
+}
 #endif
 
 /*
